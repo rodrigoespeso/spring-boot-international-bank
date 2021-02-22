d41051a35b09c25d8da0884a537c07af09123423
respesocano <respesocano@ES-PF2003B6>
2021-02-22 12:30:54 +0100

    Repositories extend from Jparepository, better than CrudRepository

--- src/main/java/com/rodrigoespeso/spbre/model/repository/AccountRepository.java
+++ src/main/java/com/rodrigoespeso/spbre/model/repository/AccountRepository.java
@@ -3,13 +3,14 @@ package com.rodrigoespeso.spbre.model.repository;
 import java.math.BigDecimal;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.rodrigoespeso.spbre.model.entity.AccountEntity;
 
 @Repository
-public interface AccountRepository extends CrudRepository<AccountEntity, BigDecimal>{
+public interface AccountRepository extends JpaRepository<AccountEntity, BigDecimal>{
 	
 	Optional<AccountEntity> findByName(String name);
 	
--- src/main/java/com/rodrigoespeso/spbre/model/repository/TmCurrencyRepository.java
+++ src/main/java/com/rodrigoespeso/spbre/model/repository/TmCurrencyRepository.java
@@ -3,13 +3,14 @@ package com.rodrigoespeso.spbre.model.repository;
 import java.math.BigDecimal;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.rodrigoespeso.spbre.model.entity.TmCurrencyEntity;
 
 @Repository
-public interface TmCurrencyRepository extends CrudRepository<TmCurrencyEntity, BigDecimal>{
+public interface TmCurrencyRepository extends JpaRepository<TmCurrencyEntity, BigDecimal>{
 	
 	Optional<TmCurrencyEntity> findByCode(String code);
 	
